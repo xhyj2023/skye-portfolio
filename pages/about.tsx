9f67061d61ec32259345ad9827328e75d277a716
xhyj2023 <147425417+xhyj2023@users.noreply.github.com>
2025-07-06 18:00:55 +0800

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,1 +1,8 @@
-关于我页代码内容略...
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I'm Skye, a developer passionate about web technologies. This site showcases some of my work.</p>
+    </div>
+  )
+}
